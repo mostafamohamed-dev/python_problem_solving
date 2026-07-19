ff46f73f03196b1b5189e0934ae9be51b284ab7d
mostafamohamed-dev <mostafahagras2005@gmail.com>
2026-07-20 00:49:54 +0300

    Create problem06.py

---
+++ problem06.py
@@ -0,0 +1,18 @@
+# Write a function<is leap>
+def is_leap(year):
+    leap = False
+    if year %400 ==0:
+        leap = True 
+        return leap
+    elif year %4==0 and year%100 !=0:
+        leap = True 
+        return leap
+    else:
+        leap = False 
+        return leap
+    
+    
+
+
+year = int(input())
+print(is_leap(year))
