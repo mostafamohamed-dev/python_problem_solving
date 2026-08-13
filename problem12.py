f0bf08b5cce4f25e8557d6343ab2fd1c28af0d62
mostafamohamed-dev <mostafahagras2005@gmail.com>
2026-08-13 03:14:33 +0300

    Create problem12.py
    generate all possible [i, j, k] combinations within the given ranges, excluding combinations where i + j + k equals n.

---
+++ problem12.py
@@ -0,0 +1,16 @@
+# lists - list comprehensions
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+
+all_list = [
+    [i, j, k]
+    for i in range(x + 1)
+    for j in range(y + 1)
+    for k in range(z + 1)
+    if i + j + k != n
+]
+print(all_list)
