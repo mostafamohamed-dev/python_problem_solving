36fdc0d761d8df8a3482b3a1a6b01d34b258960d
mostafamohamed-dev <mostafahagras2005@gmail.com>
2026-08-13 03:12:55 +0300

    Create problem11.py
    print numbers from 1 to n in decimal, octal, hexadecimal, and binary format with proper spacing.

---
+++ problem11.py
@@ -0,0 +1,8 @@
+# print formatted - decimal, octal, hexadecimal and binary
+def print_formatted(number):
+  
+    width = len(bin(number)[2:])
+    
+    
+    for i in range(1, number + 1):
+        print(f"{i:{width}d} {i:{width}o} {i:{width}X} {i:{width}b}")
